6c74524c92eb29b5e540f0e41aeb31ff0624eb53
vasujain275 <vasujain275@gmail.com>
2024-04-18 16:24:34 +0530

    Added cors and cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { booksRouter } from "./routes/books.route";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 8069;
 
@@ -8,10 +9,19 @@ const prisma = new PrismaClient();
 const app = express();
 
 // Middlewares
-app.use(express.json());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}))
+app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Routers Import
+import { booksRouter } from "./routes/books.route";
 
 
-
+// Routes
 app.use("/api/v1/books", booksRouter)
 
 
