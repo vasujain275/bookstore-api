0d19475ca878a4f7e75586e7b82208c9251f5dd5
vasujain275 <vasujain275@gmail.com>
2024-04-20 17:22:17 +0530

    feat: Added OpenAPI Schemas to Swagger Docs

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -2,6 +2,7 @@ import { version } from "../../package.json";
 import { Express, Response, Request } from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import YAML from 'yamljs';
 
 const options: swaggerJSDoc.Options = {
   definition: {
@@ -16,12 +17,18 @@ const options: swaggerJSDoc.Options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
+// Load the OpenAPI specification from the openapi.yaml file
+const openapiYaml = YAML.load('./src/openapi.yaml');
+
+// Merge the loaded YAML with the existing swaggerSpec
+Object.assign(swaggerSpec, openapiYaml);
+
 function swaggerDocs(app: Express, port: number) {
   // Swagger Page
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   // Docs in Json
-  app.get("docs.json", (req: Request, res: Response) => {
+  app.get("/docs.json", (req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
